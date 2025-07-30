90638a41de0a834623a81a51a2d63e4a986ee1a1
Ivan Jovanovic <ivojlo2002@gmail.com>
2025-07-30 17:32:06 +0200

    OL-96-modified-error-handling-and-fixed-formatting-issues-in-auth-controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -16,17 +15,13 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
         $user = User::where('email', $request->email)->first();
-        if (!$user) {
-            return response()->json([['message' => 'This user doesn\'t exist.']], 404);
+      if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([['message' => 'Your credentials are incorrect, please try again']], 400);
         }
         if (!$user->is_librarian) {
             return response()->json([['message' => 'This user cannot perform this operation']], 403);
         }
-        if (!Hash::check($request->password, $user->password)) {
-            return response()->json([['message' => 'Your credentials are incorrect, please try again']], 400);
-        }
-
-
+        
         $tokenName = 'Login token';
         $abilities = ['*'];
         $expiresAt = now()->addMonth(); 
@@ -36,8 +31,10 @@ class AuthController extends Controller
         return response()->json(['message' => 'success', 'token' => $token],200);
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         $request->user()->currentAccessToken()->delete();
+
         return response()->json(['message'=> 'You have been logged out successfully'],200);
     }
 }
