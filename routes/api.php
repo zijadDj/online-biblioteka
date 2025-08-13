33f29e2c42d2a83d7c30e3f86fa7ad5b2a9a2de1
Ivan Jovanovic <ivan@DESKTOP-U4KKEB1.home>
2025-08-13 14:55:24 +0200

    OL-59-move-genre-related-routes-to-protected-route-group

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/genre', GenreController::class);
-Route::middleware('auth:sanctum')->group(function () {
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/genre', GenreController::class);
 }
 
 );
