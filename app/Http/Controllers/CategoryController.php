4078ae3e1ff011b1aaf777854e0356b0c738e9e3
DinoDj123 <dino.djerekarac@gmail.com>
2025-08-20 00:10:44 +0200

    OL-68 adeed show function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -65,9 +65,9 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        return response()->json(["category" => $category]);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::middleware(['auth:sanctum', 'librarian'])->group(function () {
     Route::get('/books/{book}/cover', [BookController::class, 'cover']);
     Route::get('/categories',[CategoryController::class, 'index']);
     Route::post('/categories',[CategoryController::class, 'store']);
+    Route::get('/categories/{category}', [CategoryController::class, 'show']);
     Route::get('/categories/{category}/icon', [CategoryController::class, 'icon']);
     Route::put('/categories/{category}', [CategoryController::class, 'update']);
     Route::put('/categories/{category}/icon', [CategoryController::class, 'updateIcon']);
