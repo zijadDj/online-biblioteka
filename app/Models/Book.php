45781fd8f140b439d6ae949afa3194944e02dac6
DinoDj123 <dino.djerekarac@gmail.com>
2025-08-21 19:34:09 +0200

    OL-58 created database,model,relations and added publisher_id to books table

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
@@ -27,6 +29,8 @@ class Book extends Model
     {
         return $this->hasMany(Image::class);
     }
-
+    public function publishers():BelongsTo{
+        return $this->belongsTo(Publisher::class);
+    }
 
 }
---
+++ app/Models/Publisher.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Publisher extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'address',
+        'website',
+        'email',
+        'phone_number',
+        'established_year',
+    ];
+
+    public function books():HasMany{
+        return $this->hasMany(Book::class);
+    }
+
+    public function authors():BelongsToMany{
+        return $this->belongsToMany(Author::class);
+    }
+}
