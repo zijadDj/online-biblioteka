8af46136e77bc3017fb5f54c9e7ec1396081fa0b
DinoDj123 <dino.djerekarac@gmail.com>
2025-08-23 17:41:43 +0200

    OL-70 removed book id from table and model, changed relation name from book to books

--- database/migrations/2025_08_14_193411_create_categories_table.php
+++ database/migrations/2025_08_14_193411_create_categories_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->timestamps();
             $table->string('name');
             $table->string('description');
-            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
         });
     }
 
