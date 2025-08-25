d8f22f20588f76905ae46c081cb5b655688e1064
Ivan Jovanovic <ivan@DESKTOP-U4KKEB1.home>
2025-08-25 13:37:42 +0200

    OL-63_move-book-and-genres-routes-to-protected-route-group

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -20,9 +20,7 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        $params = $request->all();
-        $book = Book::create($params);
-        return $book;
+        //
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,18 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\GenreController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AuthController;
+    use App\Http\Controllers\BookController;
+    use App\Http\Controllers\GenreController;
+    use App\Http\Controllers\UserController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+    Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::apiResource('/genres', GenreController::class);
+
 Route::post('/login', [AuthController::class, 'login']);
-Route::apiResource('/books', BookController::class);
+
 Route::middleware(['auth:sanctum', 'librarian'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/users', [UserController::class, 'store']);
@@ -21,8 +21,7 @@ Route::middleware(['auth:sanctum', 'librarian'])->group(function () {
     Route::delete('/users/{user}', [UserController::class, 'destroy']);
     Route::post('/update-avatar/{user}', [UserController::class, 'updateAvatar']);
     Route::apiResource('/genres', GenreController::class);
-
-
+    Route::apiResource('/books', BookController::class);
 }
 
 );
