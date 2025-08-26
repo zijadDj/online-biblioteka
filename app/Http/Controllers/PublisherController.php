2e75b27330db31b2e26d31692ef6384034f3e2f5
DinoDj123 <dino.djerekarac@gmail.com>
2025-08-26 18:31:07 +0200

    OL-56 added missing code in show function and api.php, reformatted code

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -12,11 +12,11 @@ class PublisherController extends Controller
      */
     public function index()
     {
-        $search=request()->input('q');
-        $perPage=request()->input('paginate',20);
-        $query=Publisher::query();
-        if($search){
-            $query->where('name','like','%'.$search.'%');
+        $search = request()->input('q');
+        $perPage = request()->input('paginate', 20);
+        $query = Publisher::query();
+        if ($search) {
+            $query->where('name', 'like', '%' . $search . '%');
         }
         return response()->json($query->paginate($perPage));
     }
@@ -26,27 +26,27 @@ class PublisherController extends Controller
      */
     public function store(Request $request)
     {
-        $validated= $request->validate([
+        $validated = $request->validate([
             'name' => 'required|string|max:255',
             'address' => 'nullable|string|max:255',
-            'website'=> 'nullable|url|max:255',
-            'email'=> 'nullable|email|max:255',
-            'phone_number'=> 'nullable|string|max:255',
-            'established_year'=> 'nullable|integer'
+            'website' => 'nullable|url|max:255',
+            'email' => 'nullable|email|max:255',
+            'phone_number' => 'nullable|string|max:255',
+            'established_year' => 'nullable|integer'
         ]);
-        $publisher=Publisher::create($validated);
+        $publisher = Publisher::create($validated);
 
         return response()->json([
-            'message'=>"Publisher created successfully.",
-            "publisher"=>$publisher], 201);
+            'message' => "Publisher created successfully.",
+            "publisher" => $publisher], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Publisher $publiher)
+    public function show(Publisher $publisher)
     {
-        //
+        return response()->json($publisher);
     }
 
     /**
