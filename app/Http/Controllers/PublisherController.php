7d2d4b8fa62405933da669d68e404cdcab176336
DinoDj123 <dino.djerekarac@gmail.com>
2025-08-26 18:27:40 +0200

    OL-54 added trim function to search, changed perPage from input to integer, added validaton to perPage(20,50,100) and reformatted

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -12,11 +12,14 @@ class PublisherController extends Controller
      */
     public function index()
     {
-        $search=request()->input('q');
-        $perPage=request()->input('paginate',20);
-        $query=Publisher::query();
-        if($search){
-            $query->where('name','like','%'.$search.'%');
+        $search = trim(request()->input('q'));
+        $perPage = request()->integer('paginate', 20);
+        if (!in_array($perPage, [20, 50, 100])) {
+            return response()->json(['message' => 'Invalid per page value.'], 422);
+        }
+        $query = Publisher::query();
+        if ($search) {
+            $query->where('name', 'like', '%' . $search . '%');
         }
         return response()->json($query->paginate($perPage));
     }
@@ -26,19 +29,19 @@ class PublisherController extends Controller
      */
     public function store(Request $request)
     {
-        $validated= $request->validate([
+        $validated = $request->validate([
             'name' => 'required|string|max:255',
             'address' => 'nullable|string|max:255',
-            'website'=> 'nullable|url|max:255',
-            'email'=> 'nullable|email|max:255',
-            'phone_number'=> 'nullable|string|max:255',
-            'established_year'=> 'nullable|integer'
+            'website' => 'nullable|url|max:255',
+            'email' => 'nullable|email|max:255',
+            'phone_number' => 'nullable|string|max:255',
+            'established_year' => 'nullable|integer'
         ]);
-        $publisher=Publisher::create($validated);
+        $publisher = Publisher::create($validated);
 
         return response()->json([
-            'message'=>"Publisher created successfully.",
-            "publisher"=>$publisher], 201);
+            'message' => "Publisher created successfully.",
+            "publisher" => $publisher], 201);
     }
 
     /**
