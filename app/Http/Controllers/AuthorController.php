284aa5f41d2901663d0a1c787bcee2e83ec08596
Ivan Jovanovic <ivan@DESKTOP-U4KKEB1.home>
2025-08-27 14:41:14 +0200

    OL-72_if-check-to-make-sure-picture-exists-first

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -91,7 +91,9 @@
          */
         public function destroy(Author $author)
         {
-            Storage::disk('public')->delete($author->picture);
+            if (Storage::disk('public')->exists($author->picture)) {
+                Storage::disk('public')->delete($author->picture);
+            }
             $author->delete();
 
             return response()->json(['message' => 'Author Deleted Successfully']);
