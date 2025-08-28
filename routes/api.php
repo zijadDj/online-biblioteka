ebb877535cd70b6d1a8f5e4a8ffeb4f984513f58
Ivan Jovanovic <ivan@DESKTOP-U4KKEB1.home>
2025-08-28 13:36:01 +0200

    add_all_routes

--- routes/api.php
+++ routes/api.php
@@ -2,29 +2,25 @@
 
     use App\Http\Controllers\AuthController;
     use App\Http\Controllers\AuthorController;
+    use App\Http\Controllers\BookController;
     use App\Http\Controllers\GenreController;
     use App\Http\Controllers\UserController;
-    use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Route;
 
-    Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'login']);
-
-
-
-Route::middleware(['auth:sanctum', 'librarian'])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/users', [UserController::class, 'store']);
-    Route::get('/users', [UserController::class, 'index']);
-    Route::put('/user/{user}', [UserController::class, 'update']);
-    Route::delete('/users/{user}', [UserController::class, 'destroy']);
-    Route::post('/update-avatar/{user}', [UserController::class, 'updateAvatar']);
-    Route::apiResource('/genres', GenreController::class);
-    Route::apiResource('/authors', AuthorController::class);
-    Route::post('/authors/update-image/{author}', [AuthorController::class, 'updateAvatar']);
-}
-
-);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware(['auth:sanctum', 'librarian'])->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/users', [UserController::class, 'store']);
+        Route::get('/users', [UserController::class, 'index']);
+        Route::get('/users/{user}', [UserController::class, 'show']);
+        Route::put('/user/{user}', [UserController::class, 'update']);
+        Route::delete('/users/{user}', [UserController::class, 'destroy']);
+        Route::post('/update-avatar/{user}', [UserController::class, 'updateAvatar']);
+        Route::apiResource('books', BookController::class);
+        Route::post('/books/{book}/cover', [BookController::class, 'store']);
+        Route::apiResource('genres', GenreController::class);
+        Route::get('/books/{book}/cover', [BookController::class, 'showCover']);
+        Route::post('update-cover/{book}', [BookController::class, 'updateCover']);
+        Route::apiResource('authors', AuthorController::class);
+    });
