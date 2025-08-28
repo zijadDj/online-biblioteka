8a19a372dbdce83166c3812fcbd8b63ce8368ea4
DinoDj123 <dino.djerekarac@gmail.com>
2025-08-28 19:43:29 +0200

    OL-67 added check if icon_path is set, changed category['icon_path'] to category->icon_path, removed category->delete in if and added name of category that is deleted in json

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,16 +13,17 @@ class CategoryController extends Controller
      */
     public function index(Request $request)
     {
-        $perPage = $request->input('per_page',20);
-        $perPage= in_array($perPage,[20,50,100]) ? $perPage : 20;
-        $search = strtolower($request->input('search-value',''));
+        $perPage = $request->input('per_page', 20);
+        $perPage = in_array($perPage, [20, 50, 100]) ? $perPage : 20;
+        $search = strtolower($request->input('search-value', ''));
 
         $query = Category::query()->when($search, function ($q) use ($search) {
-                $q->whereRaw('LOWER(name) LIKE ?', ["%{$search}%"])
+            $q->whereRaw('LOWER(name) LIKE ?', ["%{$search}%"])
                 ->orWhereRaw('LOWER(description) LIKE ?', ["%{$search}%"]);
         });
         return response()->json($query->paginate($perPage));
     }
+
     /**
      * Store a newly created resource in storage.
      */
@@ -32,7 +33,7 @@ class CategoryController extends Controller
             'name' => 'required|string|max:500',
             'description' => 'required|string|max:500',
             'icon' => 'nullable|image|max:5120',
-            'book_ids'=>'nullable|array',
+            'book_ids' => 'nullable|array',
         ]);
         $iconPath = "default.jpg";
 
@@ -45,12 +46,12 @@ class CategoryController extends Controller
             'icon_path' => $iconPath,
         ]);
 
-        if(!empty($validated['book_ids'])){
+        if (!empty($validated['book_ids'])) {
             $category->books()->attach($validated['book_ids']);
         }
         return response()->json([
-            'message' => 'Category created successfully.',
-            'category' => $category]
+                'message' => 'Category created successfully.',
+                'category' => $category]
             , 201);
     }
 
@@ -76,9 +77,9 @@ class CategoryController extends Controller
             'name' => 'sometimes|string|max:500',
             'description' => 'sometimes|string|max:500',
             'icon' => 'sometimes|image|max:5120',
-            'book_ids'=>'sometimes|array',
+            'book_ids' => 'sometimes|array',
         ]);
-        $iconPath = $category['icon_path'] ;
+        $iconPath = $category['icon_path'];
 
         if ($request->hasFile('icon')) {
             $iconPath = $request->file('icon')->store('icons', 'public');
@@ -87,7 +88,7 @@ class CategoryController extends Controller
 
         $category->update($validated);
 
-        if(!empty($validated['book_ids'])){
+        if (!empty($validated['book_ids'])) {
             $category->books()->sync($validated['book_ids']);
         }
         return response()->json([
@@ -112,22 +113,18 @@ class CategoryController extends Controller
             'category' => $category->fresh(),
         ], 200);
     }
+
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Category $category)
     {
-        if (Storage::disk('public')->exists($category['icon_path'])) {
-            Storage::disk('public')->delete($category['icon_path']);
-            $category->delete();
-            return response()->json([
-                'message' => 'Category and icon deleted successfully.',
-            ], 200);
+        if ($category->icon_path && Storage::disk('public')->exists($category->icon_path)) {
+            Storage::disk('public')->delete($category->icon_path);
         }
-
         $category->delete();
         return response()->json([
-            'message' => 'Category deleted successfully.',
+            'message' => "Category: $category->name deleted successfully.",
         ]);
     }
 }
