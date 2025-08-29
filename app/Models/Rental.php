6bfaabe3469b322856c9a344d618f8715e8d1b76
Zijad Djecevic <zijad.djecevic@gmail.com>
2025-08-30 01:45:59 +0200

    OL-47 Added a factory for rentals and modified the seeder.

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Rental extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'book_id',
         'student_id',
