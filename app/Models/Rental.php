eca8deb8ad6367ba7e43b0dee6694dba95202cf6
Vuk Kapris <kaprisvuk@gmail.com>
2025-08-30 17:27:59 +0200

    Resolved merge conflict in Rental.php

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Carbon\Carbon;
 
 class Rental extends Model
@@ -14,32 +15,32 @@ class Rental extends Model
         'rented_at',
         'returned_at',
     ];
+
     protected $appends = ['days_rented', 'is_overdue'];
 
-
-    public function book()
+    public function book(): BelongsTo
     {
         return $this->belongsTo(Book::class);
     }
 
-    public function student()
+    public function student(): BelongsTo
     {
         return $this->belongsTo(User::class, 'student_id');
     }
 
-    public function librarian()
+    public function librarian(): BelongsTo
     {
         return $this->belongsTo(User::class, 'librarian_id');
     }
+
     public function getDaysRentedAttribute()
     {
-
         return Carbon::parse($this->rented_at)->diffInDays(now(), false);
     }
 
     public function getIsOverdueAttribute()
     {
-        $limitDays = 10; //
+        $limitDays = 10;
         return $this->days_rented > $limitDays;
     }
 }
