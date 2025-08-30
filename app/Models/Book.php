a49a44a2dca09e57057088f0cadfb809630cd25e
Vuk Kapris <kaprisvuk@gmail.com>
2025-08-30 17:14:48 +0200

    Resolve merge conflict in Book.php: combine OL-50 and OL-79 changes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,17 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     protected $fillable = [
-        'title',
-        'author',
-        'published_year',
+        'name',
+        'description',
+        'page_count',
+        'unit_count',
+        'isbn',
+        'language',
+        'binding',
+        'script',
+        'dimensions',
         'available_copies',
     ];
 
-    public function rentals()
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function rentals(): HasMany
     {
         return $this->hasMany(Rental::class);
     }
