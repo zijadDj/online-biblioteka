82672e2f7c86e58a49aa7c8e1ed5d2121242ff02
Vuk Kapris <kaprisvuk@gmail.com>
2025-08-30 18:00:00 +0200

    Resolved merge conflict in routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RentalController;
 use App\Models\Rental;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
 
 Route::post('/rentals', [RentalController::class, 'store']);
 Route::get('/rentals/{id}', function ($id) {
@@ -23,12 +26,17 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum', 'librarian'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/users', [UserController::class, 'store']);
-    Route::get('/policies',[PolicyController::class,'index']);
-    Route::get('/policies/{id}',[PolicyController::class,'show']);
-    Route::post('/policies',[PolicyController::class,'store']);
-    Route::put('/policies/{id}',[PolicyController::class,'update']);
-    Route::delete('/policies/{id}',[PolicyController::class,'destroy']);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::put('/user/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    Route::post('/update-avatar/{user}', [UserController::class, 'updateAvatar']);
+    Route::apiResource('books', BookController::class);
+    Route::post('/books/{book}/cover', [BookController::class, 'store']);
+    Route::apiResource('genres', GenreController::class);
+    Route::get('/books/{book}/cover', [BookController::class, 'showCover']);
+    Route::post('update-cover/{book}', [BookController::class, 'updateCover']);
+    Route::apiResource('authors', AuthorController::class);
+    Route::post('author-avatar/{author}', [AuthorController::class, 'updateAvatar']);
+});
 
-}
-
-);
