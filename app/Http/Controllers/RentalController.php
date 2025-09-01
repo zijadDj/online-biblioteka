014cda6298f0a19a9a103151526aa216a2bd74b9
Zijad Djecevic <zijad.djecevic@gmail.com>
2025-09-01 12:15:11 +0200

    OL-43 Added a paginated endpoint for all the overdue books with search

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -44,4 +44,25 @@ class RentalController extends Controller
         return RentalResource::collection($rentals);
     }
 
+    public function overdue(RentalIndexRequest $request)
+    {
+        $maxDays = 14;
+
+        $query = Rental::with(['book', 'student', 'librarian'])
+            ->whereNull('returned_at')
+            ->where('rented_at', '<', now()->subDays($maxDays));
+
+        if($search = $request->input('search')) {
+            $query->whereHas('book', function($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%");
+            });
+        }
+
+        $perPage = $request->input('per_page', 20);
+
+        $rentals = $query->paginate($perPage);
+
+        return RentalResource::collection($rentals);
+    }
+
 }
--- app/Http/Resources/RentalResource.php
+++ app/Http/Resources/RentalResource.php
@@ -9,14 +9,19 @@ class RentalResource extends JsonResource
 {
     public function toArray($request)
     {
+        $daysRented = Carbon::parse($this->rented_at)->diffInDays(now());
+        $maxDays = 14;
+        $overdue = $daysRented - $maxDays;
+
         return [
             'id' => $this->id,
             'book_title' => $this->book->name,
             'rented_by' => $this->student->name,
             'librarian' => $this->librarian->name,
             'rented_at' => $this->rented_at,
-            'active_days' => Carbon::parse($this->rented_at)->diffInDays(now()),
+            'active_days' => $daysRented,
             'returned_at' => $this->returned_at,
+            'days_overdue' => $overdue > 0 ? $overdue : 0,
         ];
     }
 }
