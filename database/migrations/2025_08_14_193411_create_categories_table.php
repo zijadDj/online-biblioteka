a67d7c56cb9fa18fbebffcf1903389f5e90bfd32
Ana0312 <ana.jokic@codeus.me>
2025-09-01 13:33:14 +0200

    Fix categories down method

--- database/migrations/2025_08_14_193411_create_categories_table.php
+++ database/migrations/2025_08_14_193411_create_categories_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('book_categories');
+        Schema::dropIfExists('categories');
     }
 };
