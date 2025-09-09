1f09fb1066e346cb12d579d6a656d4b80470ebc3
Zijad Djecevic <zijad.djecevic@gmail.com>
2025-09-09 18:33:21 +0200

    OL-41 Brought back the original logic for searching only the rented books.

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -14,7 +14,8 @@ class RentalController extends Controller
 {
     public function rented(RentalIndexRequest $request)
     {
-        $query = Rental::with(['book', 'student', 'librarian']);
+        $query = Rental::with(['book', 'student', 'librarian'])
+            ->whereNull('returned_at');
 
         if ($search = $request->input('search')) {
             $query->whereHas('book', function($q) use ($search) {
